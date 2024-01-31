530df5b2f98edc7c90e2e32a34fac37fac9460fc
Diego Modolo Ribeiro <diego.ribeiro@nectarnet.com.br>
2024-01-31 17:52:47 -0300

    Initial

---
+++ HangfireExample/CustomJobActivator.cs
@@ -0,0 +1,18 @@
+﻿using Hangfire;
+
+namespace HangfireExample;
+
+public class CustomJobActivator : JobActivator
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public CustomJobActivator(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    public override object ActivateJob(Type jobType)
+    {
+        return _serviceProvider.GetService(jobType);
+    }
+}
---
+++ HangfireExample/Program.cs
@@ -0,0 +1,37 @@
+using Hangfire;
+using HangfireExample;
+using HangfireExample.Framework;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHangfire(configuration => configuration.SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
+    .UseSimpleAssemblyNameTypeSerializer()
+    .UseRecommendedSerializerSettings()
+    .UseSqlServerStorage(builder.Configuration.GetConnectionString("HangfireConnection")));
+
+builder.Services.AddLogging();
+
+// GlobalConfiguration.Configuration.UseActivator(new CustomJobActivator(builder.Services.BuildServiceProvider()));
+
+builder.Services.AddHangfireServer(options =>
+{
+    options.Activator = new CustomJobActivator(builder.Services.BuildServiceProvider());
+});
+
+builder.Services.AddScoped<IBackgroundJob, CustomJob>();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+
+app.UseHangfireDashboard();
+
+var backgroundJobClient = app.Services.GetService<IBackgroundJobClient>();
+backgroundJobClient.Enqueue(() => Console.WriteLine("Hello world from Hangfire!"));
+backgroundJobClient.Enqueue<IBackgroundJob>(x => x.Execute("19"));
+
+RecurringJob.AddOrUpdate("say-hello", () => Console.WriteLine("Hello world from Hangfire!"), Cron.Minutely);
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
